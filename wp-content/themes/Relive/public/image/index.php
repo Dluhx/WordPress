e7cc323bfb5bbb7986da357b3737bf852bb2904c
Dluhx <im.kaychen@gmail.com>
2020-04-03 03:31:24 +0800

    Signed-off-by: Dluhx <im.kaychen@gmail.com>

--- wp-content/themes/Relive/public/image/index.php
+++
@@ -1,1 +0,0 @@
-<?php include( 'toImage.php' );?>
--- wp-content/themes/Relive/template-parts/modular/stick/full-20.php
+++
@@ -1,5 +0,0 @@
-<div class="container container--wide">
-<div class="block-heading block-heading--line">
-	<h4 class="block-heading__title">此模块仅限于调用指定分类文章</h4>
-</div>
-</div>
